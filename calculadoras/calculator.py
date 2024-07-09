4099e737b6523a81385154824985373e4ab7f553
IsabelleMay <isabellemayarast@gmail.com>
2024-07-09 12:52:54 -0300

    Initial commit

---
+++ calculadoras/calculator.py
@@ -0,0 +1,15 @@
+# Solicita entrada dos usuários
+number_1 = float(input('Digite o primeiro número: '))
+number_2 = float(input('Digite o segundo número: '))
+
+# Realiza operações
+soma = number_1 + number_2
+subtracao = number_1 - number_2
+multiplicacao = number_1 * number_2
+divisao = number_1 / number_2
+
+# Exibe resultados
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
